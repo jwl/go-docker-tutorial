1e753c97aa1ebf35316ccb61a7e568ac965f2ad6
jleung <jleung@riotgames.com>
2019-11-19 18:01:12 -0800

    Starting to add more endpoints

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # go-docker-tutorial
-Simple containerized golang API created by following [this](https://levelup.gitconnected.com/docker-for-go-development-a27141f36ba9) tutorial by Dirk Hoekstra.
+Simple containerized golang API created by following several Golang tutorials:
+
+* https://levelup.gitconnected.com/docker-for-go-development-a27141f36ba9
+* https://medium.com/codezillas/building-a-restful-api-with-go-part-1-9e234774b14d
 
 ### Setup
 * From project root, run: `docker build -t go-docker-image .`
 
 ### Usage
 * run the following command: `docker-compose up`
+* server should now be listening on port 5000 on `localhost`
--- commands/runserver.go
+++ commands/runserver.go
@@ -4,15 +4,49 @@ import (
   "fmt"
   "github.com/gorilla/mux"
   "net/http"
+  "log"
+  "encoding/json"
 )
+
+type Pokemon struct {
+  Number  string `json:"id"`
+  Name    string `json:"name"`
+}
+
+var pokemonList []Pokemon
 
 func main() {
   r := mux.NewRouter()
 
+  // endpionts
   r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "API response!\nNow on multiple lines.")
   })
+  r.HandleFunc("/pokemon", getAllPokemon).Methods("GET")
+  r.HandleFunc("/pokemon/{id}", getPokemon).Methods("GET")
+  r.HandleFunc("/pokemon/", addPokemon).Methods("POST")
+  r.HandleFunc("/pokemon/{id}", updatePokemon).Methods("POST")
+  r.HandleFunc("/pokemon/{id}", deletePokemon).Methods("DELETE")
 
   fmt.Println("Server listening!")
-  http.ListenAndServe(":80", r)
+  log.Fatal(http.ListenAndServe(":5000", r))
 }
+
+func getAllPokemon(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+  json.NewEncoder(w).Encode(pokemonList)
+}
+
+func getPokemon(w http.ResponseWriter, r *http.Request) {
+}
+
+func addPokemon(w http.ResponseWriter, r *http.Request) {
+}
+
+func updatePokemon(w http.ResponseWriter, r *http.Request) {
+}
+
+func deletePokemon(w http.ResponseWriter, r *http.Request) {
+}
+
+
